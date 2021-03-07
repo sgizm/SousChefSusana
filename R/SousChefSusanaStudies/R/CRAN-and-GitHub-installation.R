b3b58e92953f25a86ee30688e4b1c2c7476bebbe
antonocube <antononcube@gmail.com>
2021-03-07 16:17:21 -0500

    First version.

---
+++ R/SousChefSusanaStudies/R/CRAN-and-GitHub-installation.R
@@ -0,0 +1,11 @@
+
+install.packages("tidyverse", "rmarkdown", "shiny", "flexdashboard", "DT", "stopwords", "SnowballC", "NutrienTrackeR", "d3heatmap")
+
+devtools::install_github( repo = "antononcube/R-projects", subdir = "ParetoPrincipleAdherence")
+devtools::install_github( repo = "antononcube/R-projects", subdir = "OutlierIdentifiers")
+devtools::install_github( repo = "antononcube/R-projects", subdir = "SparseMatrixRecommender")
+devtools::install_github( repo = "antononcube/R-projects", subdir = "SMRMon")
+devtools::install_github( repo = "antononcube/R-projects", subdir = "SparseMatrixRecommenderInterfacesNoDT")
+devtools::install_github( repo = "antononcube/R-projects", subdir = "LSAMon")
+
+
