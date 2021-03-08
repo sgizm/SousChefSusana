0f2eb28d4f3ffae346bc7255c4b30048839a9f56
sgizm <musahedat1@gmail.com>
2021-03-08 16:52:36 +0200

    wrong repo name fixed

--- R/SousChefSusanaStudies/R/CRAN-and-GitHub-installation.R
+++ R/SousChefSusanaStudies/R/CRAN-and-GitHub-installation.R
@@ -1,11 +1,11 @@
 
 install.packages("tidyverse", "rmarkdown", "shiny", "flexdashboard", "DT", "stopwords", "SnowballC", "NutrienTrackeR", "d3heatmap", "devtools")
 
-devtools::install_github( repo = "antononcube/R-projects", subdir = "ParetoPrincipleAdherence")
-devtools::install_github( repo = "antononcube/R-projects", subdir = "OutlierIdentifiers")
-devtools::install_github( repo = "antononcube/R-projects", subdir = "SparseMatrixRecommender")
-devtools::install_github( repo = "antononcube/R-projects", subdir = "SMRMon")
-devtools::install_github( repo = "antononcube/R-projects", subdir = "SparseMatrixRecommenderInterfacesNoDT")
-devtools::install_github( repo = "antononcube/R-projects", subdir = "LSAMon")
+devtools::install_github( repo = "antononcube/R-packages", subdir = "ParetoPrincipleAdherence")
+devtools::install_github( repo = "antononcube/R-packages", subdir = "OutlierIdentifiers")
+devtools::install_github( repo = "antononcube/R-packages", subdir = "SparseMatrixRecommender")
+devtools::install_github( repo = "antononcube/R-packages", subdir = "SMRMon")
+devtools::install_github( repo = "antononcube/R-packages", subdir = "SparseMatrixRecommenderInterfacesNoDT")
+devtools::install_github( repo = "antononcube/R-packages", subdir = "LSAMon")
 
 
