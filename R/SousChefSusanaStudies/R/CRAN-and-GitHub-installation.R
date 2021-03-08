1381f44734bbe920f21dfbac0ff253a235fbd021
antonocube <antononcube@gmail.com>
2021-03-08 09:59:25 -0500

    Corrected repository sub-directory names .

--- R/SousChefSusanaStudies/R/CRAN-and-GitHub-installation.R
+++ R/SousChefSusanaStudies/R/CRAN-and-GitHub-installation.R
@@ -4,8 +4,8 @@ install.packages("tidyverse", "rmarkdown", "shiny", "flexdashboard", "DT", "stop
 devtools::install_github( repo = "antononcube/R-packages", subdir = "ParetoPrincipleAdherence")
 devtools::install_github( repo = "antononcube/R-packages", subdir = "OutlierIdentifiers")
 devtools::install_github( repo = "antononcube/R-packages", subdir = "SparseMatrixRecommender")
-devtools::install_github( repo = "antononcube/R-packages", subdir = "SMRMon")
+devtools::install_github( repo = "antononcube/R-packages", subdir = "SMRMon-R")
 devtools::install_github( repo = "antononcube/R-packages", subdir = "SparseMatrixRecommenderInterfacesNoDT")
-devtools::install_github( repo = "antononcube/R-packages", subdir = "LSAMon")
+devtools::install_github( repo = "antononcube/R-packages", subdir = "LSAMon-R")
 
 
