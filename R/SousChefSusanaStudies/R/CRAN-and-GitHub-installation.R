486412805cc20614eafd8cd658e8f112d0808f6f
antonocube <antononcube@gmail.com>
2021-03-08 11:01:19 -0500

    Added NNMF.

--- R/SousChefSusanaStudies/R/CRAN-and-GitHub-installation.R
+++ R/SousChefSusanaStudies/R/CRAN-and-GitHub-installation.R
@@ -1,11 +1,12 @@
 
-install.packages("tidyverse", "rmarkdown", "shiny", "flexdashboard", "DT", "stopwords", "SnowballC", "NutrienTrackeR", "d3heatmap", "devtools")
+install.packages("tidyverse", "rmarkdown", "shiny", "flexdashboard", "DT", "stopwords", "SnowballC", "irlba", "NutrienTrackeR", "d3heatmap", "devtools")
 
 devtools::install_github( repo = "antononcube/R-packages", subdir = "ParetoPrincipleAdherence")
 devtools::install_github( repo = "antononcube/R-packages", subdir = "OutlierIdentifiers")
 devtools::install_github( repo = "antononcube/R-packages", subdir = "SparseMatrixRecommender")
 devtools::install_github( repo = "antononcube/R-packages", subdir = "SMRMon-R")
 devtools::install_github( repo = "antononcube/R-packages", subdir = "SparseMatrixRecommenderInterfacesNoDT")
+devtools::install_github( repo = "antononcube/R-packages", subdir = "NonNegativeMatrixFactorization")
 devtools::install_github( repo = "antononcube/R-packages", subdir = "LSAMon-R")
 
 
