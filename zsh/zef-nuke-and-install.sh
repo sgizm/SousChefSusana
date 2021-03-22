cebb2f3f56f0dadce951f0c72803c9094995b6c3
antonocube <antononcube@gmail.com>
2021-03-22 14:40:14 -0400

    First version.

---
+++ zsh/zef-nuke-and-install.sh
@@ -0,0 +1,19 @@
+
+# Comment out if there is no zef:
+zef nuke home
+
+# Comment out if there is no zef:
+zef nuke site
+
+## Edit this correct directory name of the zef repository:
+cd ~/GitHub/ugexe/zef
+
+raku -I. bin/zef install .
+
+cd -
+
+## Install Raku packages
+zef install https://github.com/antononcube/Raku-DSL-Shared.git --force-install
+zef install https://github.com/antononcube/Raku-DSL-Entity-English-Foods.git --force-install
+zef install https://github.com/antononcube/Raku-DSL-Entity-English-Geographics.git --force-install
+zef install https://github.com/antononcube/Raku-DSL-English-FoodPreparationWorkflows.git --force-install --force-test
